4617ac085d2bda13c69b37e575b9e62aeffbf4de
nutrimarketpro-mx <nutrimarketpro@gmail.com>
2026-05-08 22:19:44 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // Por seguridad
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // Por seguridad
   ],
   theme: {
     extend: {},
